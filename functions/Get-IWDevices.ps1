e7a0cae4b2913379b86eea999e0a4a6d1a8496a6
John Kischel <johnkischel@gmail.com>
2020-04-25 07:29:04 +0200

    Checking zero and m,ultiple usbdevices.

--- functions/Get-IWDevices.ps1
+++ functions/Get-IWDevices.ps1
@@ -1,13 +1,22 @@
-function Get-IWDevices
-{
+function Get-IWDevices {
     # TODO: Make a selection enable the user to choose the device.
-    try
-    {
+    try {
         $InputObject = Get-Disk | Where-Object { $_.BusType -eq 'USB' }
-        return $InputObject
-    }
-    catch
-    {
+        switch ($InputObject.Length) {
+            0 {
+                Write-PSFMessage -Level Host -Message "No usbdevice found."
+                throw [System.Exception]::new("Missing usbdevice.")
+            }
+            1 {
+                Write-PSFMessage -Level Host -Message ("USB device with SerialNumber: {0} found." -f $InputObject.SerialNumber)
+                return $InputObject
+            }
+            default {
+                Write-PSFMessage -Level Host -Message "More than one usbdevice found."
+            }
+        }
+        
+    } catch {
         Write-PSFMessage -Level Host -Message $_.Exception.Message
     }
 }
