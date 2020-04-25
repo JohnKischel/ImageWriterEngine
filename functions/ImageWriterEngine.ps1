b2a6af0ac4632ab3d0102cb6e4e309c83b85a831
John Kischel <johnkischel@gmail.com>
2020-04-25 22:29:49 +0200

    Added session remove after finishing.

--- functions/ImageWriterEngine.ps1
+++ functions/ImageWriterEngine.ps1
@@ -18,13 +18,16 @@ function Start-ImageWriterEngine {
         try {
             $Device, $DriveLetter = Get-IWDevices | Start-IWPrepareDevice -DriveLetter $DriveLetter
             $Image = Mount-IWImage -ImagePath $ImagePath
+
             Robocopy.exe $("{0}:\" -f $Image.DriveLetter) $("{0}:\" -f $DriveLetter) /S /E /W:1 /R:2
         } catch {
             Write-PSFMessage -Level Host -Message $_.Exception.Message
         }
     }
 
-    end { 
+    end {
+        # Dismount-DiskImage -InputObject (Get-PSFConfigValue -FullName ImageWriterEngine.Session.DiskImage)
+        Remove-Item -Path (Join-PSFPath (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Path) -Child (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Id)) -Force
         $ErrorActionPreference = "Continue"
     }
 }
