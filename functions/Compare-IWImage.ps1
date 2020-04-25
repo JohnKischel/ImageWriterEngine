d1f2f62d066d91527c20265c34dbdb053f55232f
John Kischel <johnkischel@gmail.com>
2020-04-25 07:28:25 +0200

    when same path is compared validate without hash

--- functions/Compare-IWImage.ps1
+++ functions/Compare-IWImage.ps1
@@ -1,5 +1,4 @@
-function Compare-IWImage
-{
+function Compare-IWImage {
     param(
         # Path to an iso image.iso
         [Parameter(Mandatory)]
@@ -10,16 +9,19 @@ function Compare-IWImage
         $DifferenceImage
     )
 
+    if ($ReferenceImage -eq $DifferenceImage) {
+        Write-PSFMessage -Level Host -Message "Pathes are identically no need to verify."
+        return $true
+    }
+    
     $ReferenceObject = Get-FileHash $ReferenceImage
     $DifferenceObject = Get-FileHash -Path $DifferenceImage
 
-    if(-not (Compare-Object -ReferenceObject $ReferenceObject.Hash -DifferenceObject $DifferenceObject.Hash))
-    {
-        Write-PSFMessage -Level Host -Message "The local and the remote iso are identically."
+    if (-not (Compare-Object -ReferenceObject $ReferenceObject.Hash -DifferenceObject $DifferenceObject.Hash)) {
+        Write-PSFMessage -Level Host -Message "The local and the remote iso is identically."
         return $true
-    }
-    else{
-        Write-PSFMessage -Level Host -Message "The local and the remote iso are different."
+    } else {
+        Write-PSFMessage -Level Host -Message "The local and the remote iso is different."
         return $false
     }
 }
