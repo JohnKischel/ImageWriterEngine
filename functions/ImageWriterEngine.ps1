970cc7d5c8b24c6e14347d97a9efd91af48c84a7
John Kischel <johnkischel@gmail.com>
2020-04-26 22:50:59 +0200

    Added robocopy log parameter.

--- functions/ImageWriterEngine.ps1
+++ functions/ImageWriterEngine.ps1
@@ -19,7 +19,7 @@ function Start-ImageWriterEngine {
             $Device, $DriveLetter = Get-IWDevices | Start-IWPrepareDevice -DriveLetter $DriveLetter
             $Image = Mount-IWImage -ImagePath $ImagePath
 
-            Robocopy.exe $("{0}:\" -f $Image.DriveLetter) $("{0}:\" -f $DriveLetter) /S /E /W:1 /R:2
+            Robocopy.exe $("{0}:\" -f $Image.DriveLetter) $("{0}:\" -f $DriveLetter) /S /E /W:1 /R:2 /Log $(Get-PSFConfigValue -FullName ImageWriterEngine.Log.Path)
         } catch {
             Write-PSFMessage -Level Host -Message $_.Exception.Message
         }
