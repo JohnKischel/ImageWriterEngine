a91ac42ff3b4714b93c0bed8a86223e9c4d30452
JohnKischel <johnkischel@gmail.com>
2020-04-27 12:18:26 +0200

    Added dismount for iso, ImageWriterEngine Path creation added

--- functions/ImageWriterEngine.ps1
+++ functions/ImageWriterEngine.ps1
@@ -1,17 +1,20 @@
 function Start-ImageWriterEngine {
     [CmdletBinding()]
     param (
-        [Parameter()]
+        [Parameter(HelpMessage="Path of the isofile.")]
         [ValidateNotNullorEmpty()]
         [String]
         $ImagePath,
 
-        [Parameter()]
+        [Parameter(HelpMessage="Select your DriveLetter from A-Z. If the driveletter is not available it is automatically selected.")]
+        [AllowEmptyString()]
         [Char]
-        $DriveLetter
+        $DriveLetter = "^"
     )
     begin {
         $ErrorActionPreference = "STOP"
+        Set-IWHardwareDetection -Stop
+        [System.IO.Directory]::CreateDirectory((Get-PSFConfigValue ImageWriterEngine.Session.Path)) | Out-Null
     }
 
     process {
@@ -20,14 +23,24 @@ function Start-ImageWriterEngine {
             $Image = Mount-IWImage -ImagePath $ImagePath
 
             Robocopy.exe $("{0}:\" -f $Image.DriveLetter) $("{0}:\" -f $DriveLetter) /S /E /W:1 /R:2
-        } catch {
+        }
+        catch {
             Write-PSFMessage -Level Host -Message $_.Exception.Message
         }
     }
 
     end {
         # Dismount-DiskImage -InputObject (Get-PSFConfigValue -FullName ImageWriterEngine.Session.DiskImage)
-        Remove-Item -Path (Join-PSFPath (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Path) -Child (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Id)) -Force
+        $sessionPath = (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Path)
+        if ($sessionPath) {
+            Remove-Item -Path (Join-Path $sessionPath -Child (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Id)) -Force -Recurse
+        }
+
+        do{
+            $result = Dismount-DiskImage -ImagePath (Get-PSFConfigValue ImageWriterEngine.Session.DiskImagePath)
+        }until(!$result)
+
+        Set-IWHardwareDetection -Start
         $ErrorActionPreference = "Continue"
     }
 }
