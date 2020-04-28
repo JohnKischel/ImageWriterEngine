50d5e0806e5f6c76f11adf5362f1be484ecafb18
johnkischel <johnkischel@gmail.com>
2020-04-28 14:11:49 +0200

    Formatted the script and added path validation.

--- functions/ImageWriterEngine.ps1
+++ functions/ImageWriterEngine.ps1
@@ -1,18 +1,30 @@
-function Start-ImageWriterEngine {
+function Start-ImageWriterEngine
+{
     [CmdletBinding()]
     param (
-        [Parameter(HelpMessage="Path of the isofile.")]
-        [ValidateNotNullorEmpty()]
+        [Parameter(HelpMessage = "Path of the isofile.")]
         [String]
         $ImagePath,
 
-        [Parameter(HelpMessage="Select your DriveLetter from A-Z. If the driveletter is not available it is automatically selected.")]
+        [Parameter(HelpMessage = "Select your DriveLetter from A-Z. If the driveletter is not available it is automatically selected.")]
         [AllowEmptyString()]
         [Char]
         $DriveLetter = "^"
     )
-    begin {
+    begin
+    {
         $ErrorActionPreference = "STOP"
+
+        if ([String]::IsNullOrEmpty($ImagePath) -or !($ImagePath -match ".+\.iso") -or !(Test-Path -Path $ImagePath))
+        {
+            $ImagePath = (Get-ChildItem -Path .\ISO\*.iso -ErrorAction 0).FullName
+            if ([String]::IsNullOrEmpty($ImagePath))
+            {
+                Write-PSFMessage -Level Host -Message "No ISO found."
+                exit
+            }
+        }	
+
         Set-IWHardwareDetection -Stop
         [System.IO.Directory]::CreateDirectory((Get-PSFConfigValue ImageWriterEngine.Session.Path)) | Out-Null
         [System.IO.Directory]::CreateDirectory(("{0}" -f (Get-PSFConfigValue -FullName ImageWriterEngine.Log.Path))).FullName
@@ -20,26 +32,32 @@ function Start-ImageWriterEngine {
 
     }
 
-    process {
-        try {
+    process
+    {
+        try
+        {
             $Device, $DriveLetter = Get-IWDevices | Start-IWPrepareDevice -DriveLetter $DriveLetter
             $Image = Mount-IWImage -ImagePath $ImagePath
             Robocopy.exe $("{0}:\" -f $Image.DriveLetter) $("{0}:\" -f $DriveLetter) /S /E /W:1 /R:2 /NP /LOG:$logfile | Out-Null
-        } catch {
+        }
+        catch
+        {
             Write-PSFMessage -Level Host -Message $_.Exception.Message
         }
 
         Add-IWBootLoader -DriveLetter $DriveLetter
     }
 
-    end {
+    end
+    {
         <#
         $sessionPath = (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Path)
         if (Test-Path $sessionPath) {
             Remove-Item -Path (Join-PSFPath $sessionPath -Child (Get-PSFConfigValue -FullName ImageWriterEngine.Session.Id)) -Force -Recurse
         }
         #>
-        do {
+        do
+        {
             $result = Dismount-DiskImage -ImagePath (Get-PSFConfigValue ImageWriterEngine.Session.DiskImagePath)
         }until(!$result)
 
