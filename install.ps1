dd52935d9a94b6c09b74244d93f4b83e87c780be
JohnKischel <johnkischel@gmail.com>
2020-05-01 23:03:07 +0200

    File isnt needed anymore. Replaced with NestedModules.

--- install.ps1
+++
@@ -1,3 +0,0 @@
-#Requires -RunAsAdministrator
-Copy-Item -Path "..\ImageWriterEngine" $env:programfiles\WindowsPowershell\Modules -Recurse -Exclude "install.ps1","lib\" -Force
-Copy-Item -Path "..\ImageWriterEngine\lib\psframework\PSFramework" $env:programfiles\WindowsPowershell\Modules -Recurse -Force
