bf9a4f3e67da19ab056088ad1636329a511f0d00
JohnKischel <johnkischel@gmail.com>
2020-05-02 23:28:30 +0200

    whitespace issue

--- functions/Boot/Add-IWBootloader.ps1
+++ functions/Boot/Add-IWBootloader.ps1
@@ -10,7 +10,7 @@ function Add-IWBootLoader {
         [string]
         $StorePath = "$(Get-PSFConfigValue ImageWriterEngine.Session.StorePath)\BCD"
     )
-
+    
     $guid = bcdedit.exe /store $StorePath /create /d $BootLoaderName /application osloader
     $Identifier = [regex]::Matches($guid, "\w{0,8}-\w{0,4}-\w{0,4}-\w{0,4}-\w{0,12}").Value
     return $("{{{0}}}" -f $Identifier)
