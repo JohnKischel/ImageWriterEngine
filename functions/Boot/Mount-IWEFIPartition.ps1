5f5d7d40f4f1d1128a21eebc72a42618b1e1474e
JohnKischel <johnkischel@gmail.com>
2020-05-05 21:06:43 +0200

    added mount process.

--- functions/Boot/Mount-IWEFIPartition.ps1
+++ functions/Boot/Mount-IWEFIPartition.ps1
@@ -6,21 +6,23 @@ function Mount-IWEFIPartition {
     )
     
     begin { 
-
+        Mount-IWEFIPartition -DriveLetter $DriveLetter
     }
 
     process {
         [System.IO.Directory]::CreateDirectory((Get-PSFConfigValue ImageWriterEngine.Session.MountPath)) | Out-Null
 
         try {
-            Get-IWDevicePartitions -DriveLetter $DriveLetter
+            Get-IWDevicePartitions -DriveLetter $DriveLetter | Out-Null
             Add-PartitionAccessPath -DiskNumber (Get-PSFConfigValue ImageWriterEngine.Session.Device).Disknumber -PartitionNumber (Get-PSFConfigValue ImageWriterEngine.Session.Device).EFIPartitionNumber -AccessPath (Get-PSFConfigValue ImageWriterEngine.Session.MountPath)
         }
         catch {
             $dismount = ("mountvol.exe {0} /D" -f (Get-PSFConfigValue ImageWriterEngine.Session.MountPath))
-            Invoke-Expression -Command $dismount
+            Invoke-Expression -Command $dismount | Out-Null
         }
     }
 
-    end { }
+    end {
+        Dismount-IWEFIPartition -DriveLetter $DriveLetter
+    }
 }   
