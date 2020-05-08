b78f1974138933e77af57d64f16531cb5a7faff7
JohnKischel <johnkischel@gmail.com>
2020-05-08 12:02:02 +0200

    added more defined error handling.

--- functions/main/Mount-IWImage.ps1
+++ functions/main/Mount-IWImage.ps1
@@ -2,40 +2,47 @@ function Mount-IWImage {
     # Path to the isoimage wich should be mounted.
     [CmdletBinding()]
     param (
-        [Parameter(HelpMessage="Path of the isofile.")]
-        [ValidateNotNullOrEmpty()]
+        [Parameter(HelpMessage = "Path of the isofile.")]
+        [AllowNull()]
         [string]
         $ImagePath = (Get-PSFConfigValue ImageWriterEngine.Session.DiskImagePath)
     )
 
     begin {
-        if (!($ImagePath -match ".+\.iso") -or !(Test-Path -Path $ImagePath))
-        {
-            $ImagePath = (Get-ChildItem -Path .\ISO\*.iso -ErrorAction 0).FullName
-            if ([String]::IsNullOrEmpty($ImagePath))
-            {
-                Write-PSFMessage -Level Host -Message "No ISO found."
-                exit
+        if([String]::IsNullOrEmpty($ImagePath)) {
+            Write-PSFMessage -Level Host -Message "Searching for image locally."
+            $ImagePath = (Get-ChildItem -Path (Join-PSFPath (Get-PSFConfigValue ImageWriterEngine.Session.Path) -Child "*.iso") -ErrorAction 0).FullName
+            if($ImagePath){
+                Write-PSFMessage -Level Host -Message ("Image: {0} found." -f $ImagePath)
             }
-        }	
+        }
+
+        if (-not ($ImagePath -match ".+\.iso") -or -not (Test-Path -Path $ImagePath)) {
+            throw ("Path doesnt match '.+\.iso' or is not available.")
+        }
     }
     process {
         try {
-            $InputObject = Get-DiskImage $ImagePath | Mount-DiskImage -StorageType ISO | Get-Volume
-            Write-PSFMessage -Level Host -Message ("Image: [ {0} ] mounted as [ {1}: ] with size [ {2:f2} ]" -f $InputObject.FileSystemLabel , $InputObject.DriveLetter, ($InputObject.Size / 1GB ))       
+            $InputObject = Get-DiskImage $ImagePath -ErrorAction Stop | Mount-DiskImage -StorageType ISO | Get-Volume
         }
         catch {
-            Write-PSFMessage -Level Host -Message ("Could not mount Image: {0}" -f $ImagePath)       
+            throw "The file or directory is corrupted and unreadable."
         }
-    
+        #       
+        if ($InputObject) {
+            Write-PSFMessage -Level Host -Message ("Image: [ {0} ] mounted as [ {1}: ] with size [ {2:f2} ]" -f $InputObject.FileSystemLabel , $InputObject.DriveLetter, ($InputObject.Size / 1GB ))       
+        }
+        else {
+            throw ("Could not mount Image: {0} the returning Object was null" -f $ImagePath )       
+        }
+        
         Set-PSFConfig -FullName ImageWriterEngine.Session.DiskImage -Value $InputObject -Description "Mounted Image as object."
         Set-PSFConfig -FullName ImageWriterEngine.Session.DiskImagePath -Value $ImagePath -Description "The path of the iso image."
 
-        if([System.IO.File]::Exists(("{0}:\Deploy\Boot\LiteTouchPE_x64.wim" -f (Get-PSFConfigValue -FullName ImageWriterEngine.Session.DiskImage).DriveLetter)))
-        {
+        if ([System.IO.File]::Exists(("{0}:\Deploy\Boot\LiteTouchPE_x64.wim" -f (Get-PSFConfigValue -FullName ImageWriterEngine.Session.DiskImage).DriveLetter))) {
             Write-PSFMessage -Level Host -Message ("WinPE detected.")       
         }
-        else{
+        else {
             throw [Exception]::new("ISO is not a WINPE.")
         }
     }
