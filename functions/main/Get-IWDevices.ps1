c9f323d58dacd24504130b40c802512c5ae0f790
JohnKischel <johnkischel@gmail.com>
2020-05-11 22:57:02 +0200

    slightly changes to iwdevices to list disks and volumes.

--- functions/main/Get-IWDevices.ps1
+++ functions/main/Get-IWDevices.ps1
@@ -11,38 +11,60 @@ function Get-IWDevices {
         # Use this switch to ignore everything except Removeable devices.
         [Parameter(ParameterSetName = "GetByDriveLetter")]
         [switch]
-        $Secure
+        $Secure,
+
+        # select this switch to list all drives with driveletter.
+        [Parameter(ParameterSetName = "ListAll")]
+        [switch]
+        $ListAll
     )
     
     begin {
-        if (-not (Test-Path -Path $("{0}:\" -f $DriveLetter))) {
-            throw ("{0}:\ was not found as path." -f $DriveLetter)
-        }  
+
     }
     
-    process { 
-        if ($Secure.IsPresent) {
-            $InputObject = Get-Volume | Where-Object { $_.DriveLetter -eq $DriveLetter -and $_.DriveType -eq "Removable" } | Get-Partition | Get-Disk
-            if ($InputObject) {
-                return $InputObject
+    process {
+        
+        switch ($PSCmdlet.ParameterSetName) {
+
+            #Gets the volume with specific driveletter.
+            "GetByDriveLetter" {
+
+                if (-not (Test-Path -Path $("{0}:\" -f $DriveLetter))) {
+                    throw ("{0}:\ was not found as path." -f $DriveLetter)
+                }  
+
+                if ($Secure.IsPresent) {
+                    $InputObject = Get-Volume | Where-Object { $_.DriveLetter -eq $DriveLetter -and $_.DriveType -eq "Removable" } | Get-Partition | Get-Disk
+                    if ($InputObject) {
+                        return $InputObject
+                    }
+                    else {
+                        throw 'Object returned was null'
+                    }
+                }
+                else {
+                    $InputObject = Get-Volume -DriveLetter $DriveLetter | Get-Partition | Get-Disk
+                    if ($InputObject) {
+                        return $InputObject
+                    }
+                    else {
+                        throw 'Object returned was null'
+                    }
+                }
+
+                Set-PSFConfig -Module 'ImageWriterEngine' -Name 'Session.DriveLetter' -Value $DriveLetter
+                Set-PSFConfig -Module 'ImageWriterEngine' -Name 'Session.DeviceInputObject' -Value $InputObject
             }
-            else {
-                throw 'Object returned was null'
+
+            # List all volumes and disks.
+            "ListAll" {
+                Get-Volume | Where-Object { $_.DriveLetter } | Format-Table @{Label="DriveLetter";Expression={$_.DriveLetter}},@{Label="FriendlyName";Expression={$_.FileSystemLabel}},@{Label="Size";Expression={$_.Size}}
+                Get-Disk | Format-Table @{Label="DiskNumber";Expression={$_.DiskNumber}},@{Label="FriendlyName";Expression={$_.FriendlyName}}
             }
         }
-        else {
-            $InputObject = Get-Volume -DriveLetter $DriveLetter | Get-Partition | Get-Disk
-            if ($InputObject) {
-                return $InputObject
-            }
-            else {
-                throw 'Object returned was null'
-            }
-        }
-            
     }
     end { 
-        Set-PSFConfig -Module 'ImageWriterEngine' -Name 'Session.DriveLetter' -Value $DriveLetter
-        Set-PSFConfig -Module 'ImageWriterEngine' -Name 'Session.DeviceInputObject' -Value $InputObject
+
     }
 }
