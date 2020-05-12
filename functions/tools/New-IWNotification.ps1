3fdcc9a2164122cbde8d8ee97ad4158677853424
JohnKischel <johnkischel@gmail.com>
2020-05-12 07:33:12 +0200

    Added notification.

---
+++ functions/tools/New-IWNotification.ps1
@@ -0,0 +1,26 @@
+function New-IWNotification
+{
+    [CmdletBinding()]
+    param (
+
+        # Message title
+        [Parameter()]
+        [string]
+        $Title = "ImageWriterEngine",
+
+        # Message body
+        [Parameter()]
+        [String]
+        $Message
+
+    )
+    Add-Type -AssemblyName System.Windows.Forms
+    $global:notify = New-Object System.Windows.Forms.NotifyIcon
+    $path = (Get-Process -id $pid).Path
+    $notify.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+    $notify.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+    $notify.BalloonTipText = $Message
+    $notify.BalloonTipTitle = $Title
+    $notify.Visible = $true
+    $notify.ShowBalloonTip(20000) 
+}
