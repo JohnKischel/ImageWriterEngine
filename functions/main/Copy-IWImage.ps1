47cde8080c31d77456c34490ff7143f6244ab09a
JohnKischel <johnkischel@gmail.com>
2020-05-14 13:40:49 +0200

    Change Start-Job -> Start-ThreadJob.

--- functions/main/Copy-IWImage.ps1
+++ functions/main/Copy-IWImage.ps1
@@ -21,7 +21,7 @@ function Copy-IWImage {
     )
 
     process {
-        Start-Job -ScriptBlock {
+        Start-ThreadJob -ScriptBlock {
             param($ImageDriveLetter, $LogFile, $DriveLetter)
             Robocopy.exe $("{0}:\" -f $ImageDriveLetter) $("{0}:\" -f $DriveLetter) /S /E /MIR /W:1 /R:2 /NP /LOG:$logfile | Out-Null 
         } -ArgumentList $ImageDriveLetter, $LogFile, $DriveLetter -Name ImageCopy | Out-Null
